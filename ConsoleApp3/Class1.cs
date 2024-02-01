00553a3d4cf70b890fa5ac382060c1b7fa4dace6
Cupsuki001 <124387400+Cupsuki001@users.noreply.github.com>
2024-02-01 11:27:28 -0600

    Add files via upload

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+        public int validar(String Mensaje)
+        {
+            int num = 0, v;
+            do
+            {
+                v = 1;
+                try
+                {
+                    Console.Write(Mensaje);
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+                    Console.WriteLine("No se aceptan letras", error.Message);
+                }
+            } while (v == 0);
+            return num;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp3;
+
+public class Program
+{
+    public static void Main(string[] args) 
+    {
+        Class1 n = new Class1();
+        int p;
+        double v , ttv , e , tte , d , ttd , sal;
+        do 
+        {
+
+            Console.WriteLine("Ingrese una opcion");
+            Console.WriteLine("1.op Vehiculo");
+            Console.WriteLine("2.op Equipo de computo");
+            Console.WriteLine("3.op Edificio");
+            Console.WriteLine("4.op Salir");
+            p = int.Parse(Console.ReadLine());
+
+            switch (p) 
+            {
+               case 1:
+                    {
+
+                        do
+                        {
+                            v = n.validar("Ingrese el valor del Vehiculo: ");
+
+                        } while (v <= 10000 || v > 20000);
+                        ttv = v / 5;
+                        Console.WriteLine("La depreciacion es de: " + ttv);
+                    }
+                    break;
+
+
+                case 2:
+                    {
+
+                        do
+                        {
+                            e = n.validar("Ingrese el valor del Equipo de computo: ");
+                        } while (e <= 20000 || e > 50000);
+                        tte = e / 2;
+                        Console.WriteLine("La depreciacion es de: " + tte);
+                        break;
+                    }
+
+                case 3: 
+                    {
+                        do
+                        {
+                            d = n.validar("Ingrese el valor del Edificio: ");
+                        } while (d < 200000 || d > 90000);
+                        ttd = d / 20;
+                        Console.WriteLine("La depreciacion es de: " + ttd);
+                        break;
+
+                    }
+
+
+            }
+            Console.WriteLine("mamyuca");
+
+
+        } while (p == 4);
+    }
+}
